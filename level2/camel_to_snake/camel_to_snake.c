9d4b83730e0f56170540f86a9097f79b6e1c03b4
OnurNigiz <onurnigiz@hotmail.com>
2023-08-08 13:26:05 +0300

    done!

---
+++ level2/camel_to_snake/camel_to_snake.c
@@ -0,0 +1,72 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int flen(char *str)
+{
+    int i = 0;  
+    while (str[i])
+        i++;
+    return (i);
+}
+
+char *re_write(char *str, char *str2)
+{
+    int i = 0;
+    int x = 0;
+    while (str[i])
+    {   
+        if(str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str2[x] = '_';
+            x++;
+            str[i] += 32;
+        }
+        
+        str2[x] = str[i];
+        i++;
+        x++;
+    }
+    str2[x] = '\0';
+    return (str2);
+}
+
+int word_counter(char *str)
+{
+    int i = 0;
+    int c = 0;
+    while (str[i])
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+            c++;
+        i++;
+    }
+    return c;
+}
+
+char putstr(char *str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+    if(argc == 2)
+    {
+        int words = word_counter(argv[1]);
+        char *handle = (char *)malloc(flen(argv[1]) + words + 1);
+        if(!handle)
+            return (0);
+        handle = re_write(argv[1], handle);
+        putstr(handle);
+        free(handle);
+    }
+    write(1, "\n", 1);
+    return (0);
+
+}
