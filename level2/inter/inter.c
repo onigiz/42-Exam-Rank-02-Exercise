4288a78a778cf490061b183d8e65b2e880b6fe69
OnurNigiz <onurnigiz@hotmail.com>
2023-08-10 13:42:44 +0300

    done!

--- level2/inter/inter.c
+++ level2/inter/inter.c
@@ -1,63 +1,91 @@
 #include <unistd.h>
+#include <stdio.h>
 
-int fstrlen(char *str)
+//Print function
+char *fputstr(char *str)
 {
     int i = 0;
     while (str[i])
+    {
+        write(1, &str[i], 1);
         i++;
+    }    
+}
+
+//String len?
+int flen(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
     return (i);
 }
 
-int chehcker(char x, char *check_list)
+//Could we write the char on "will be write" list?
+int is_there(char *check_list, char c)
 {
     int i = 0;
     while (check_list[i])
     {
-        if(x == check_list[i])
+        if (c == check_list[i])
+        {
             return (1);
+        }
         i++;
     }
     return (0);
 }
 
-void list_adder(char *check_list, char c)
+//Current "will be write" list
+void write_that(char *check_list, char c)
 {
-    int len = fstrlen(check_list);
-    check_list[len] = c;
-    check_list[len +1] = '\0';
+    if(is_there(check_list, c) == 0)
+    {
+        int len = flen(check_list);
+        check_list[len] = c;
+    }
 }
 
-#include <stdio.h>
 
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-    if(argc == 3)
+    if (argc == 3)
     {
-        char *check_list;
+        int max_size = flen(argv[2]);
+
+        char check_list[max_size];
         int i = 0;
-        
+        while (i < max_size)
+        {
+            check_list[i] = 0;
+            i++;
+        }
+
+        i = 0;
         while (argv[1][i])
         {
-            printf("Döngü 1\n");
             int c = 0;
             while (argv[2][c])
             {
-                
-                if(argv[1][i] == argv[2][c])
-                {
-                    if (chehcker(argv[1][i], check_list) == 0)
-                    {
-                        list_adder(check_list, argv[1][i]);
-                        printf("list: %s", check_list);
-                    }
-                    write(1, &argv[1][i], 1);
-                }
+                if (argv[1][i] == argv[2][c])
+                    write_that(check_list, argv[1][i]);
                 c++;
             }
-            i++;    
+            i++;
         }
-        
+
+        fputstr(check_list);
     }
     write(1, "\n", 1);
-    return (0);
+    return 0;
 }
+
+//check_list'in mainindeki tanımını yaparken sabit sayılı bir dizi atamazsam 
+//seg fault oluyor. Malloc kullanımına izin olmadığından böyle bir yöntem kullanıyorum.
+//BAşlangıçta tüm diziyi null doldurup daha sonra manipule ediyorum. Zaten başta tüm 
+//array null doldurulduğu için sona null eklememe gerek yok!
+//write_that fonksiyonu void tanımlanıp arrayde manipulasyon yapıyor sadece.
+//Bu fonksiyonla birlikte tekrar tekrar checkliste atama yapmaya gerek yok, onu handle edemedim.
